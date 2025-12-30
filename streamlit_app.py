9668652ee490d2d69cb9fdde17fe9568b493159c
Ruben Ferro <93170378+RubenFerro@users.noreply.github.com>
2025-12-30 11:23:34 -0500

    Add Snowflake connection and session management
    Establish Snowflake connection and session for data retrieval.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -12,9 +15,7 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be: ', name_on_order)
 
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -39,5 +40,3 @@ if ingredients_list:
             'Your Smoothie is ordered, ' + name_on_order + '!',
             icon="✅"
     )
-cnx = st.connection("snowflake")
-session = cnx.session()
